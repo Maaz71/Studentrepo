81ae8990bc62124b7a7ecaf69410ee36e304b094
kunal <kunalpatil6516@gmail.com>
2023-10-21 15:39:11 +0530

    We have commit Student Project

---
+++ Student.h
@@ -0,0 +1,31 @@
+#include<iostream>
+class Student
+{
+	int studid;
+	string studName;
+	int mark1;
+	int mark2;
+	inty mark3;
+	public:
+		Student()
+		{
+			this.studid=101;
+			this.studName="Rahul"
+			this.mark1=70;
+			this.mark2=70;
+			this.mark3=70;
+		}
+		
+		
+		int getStudid
+		{
+		}
+		string getName()
+		{
+		}
+		
+		void calculatePer()
+		{
+		}
+		
+}
